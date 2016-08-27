8f057b3a2230847e70a111cbbcbbfe78a51c2883
Ian Ward <ian@excess.org>
2016-08-27 15:54:30 -0400

    [#3192] example_ipermissionlabels extension

--- setup.py
+++ setup.py
@@ -136,6 +136,7 @@ entry_points = {
         'example_itranslation = ckanext.example_itranslation.plugin:ExampleITranslationPlugin',
         'example_iconfigurer_v1 = ckanext.example_iconfigurer.plugin_v1:ExampleIConfigurerPlugin',
         'example_iconfigurer_v2 = ckanext.example_iconfigurer.plugin_v2:ExampleIConfigurerPlugin',
+        'example_ipermissionlabels = ckanext.example_ipermissionlabels:ExampleIPermissionLabels',
     ],
     'ckan.system_plugins': [
         'domain_object_mods = ckan.model.modification:DomainObjectModificationExtension',
